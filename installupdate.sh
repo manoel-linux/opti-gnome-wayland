6f97602b881710eea7ccb2fc156aa3905ad71bc3
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-06-17 01:38:51 -0300

    Add files via upload

---
+++ installupdate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+clear
+echo "#################################################################"
+echo "opti-gnome-wayland-installer: june 2023"
+echo "#################################################################"
+echo "  ██████  ███    ██  ██████  ███    ███ ███████ "
+echo " ██       ████   ██ ██    ██ ████  ████ ██      "
+echo " ██   ███ ██ ██  ██ ██    ██ ██ ████ ██ █████   "
+echo " ██    ██ ██  ██ ██ ██    ██ ██  ██  ██ ██      "
+echo "  ██████  ██   ████  ██████  ██      ██ ███████ "                                                                                            
+echo "#################################################################"
+echo "opti-gnome-wayland-github: https://github.com/none/none/none"
+echo "#################################################################"
+echo "$(date)"
+echo "Linux $(uname -r)"
+echo "#################################################################"
+
+if [[ $EUID -ne 0 ]]; then
+echo "ERROR! sudo needed" 
+echo "#################################################################"
+exit 1
+fi
+
+rm /usr/bin/opti-gnome-wayland
+
+cp opti-gnome-wayland /usr/bin/
+
+chmod +x /usr/bin/opti-gnome-wayland
+
+echo "DONE! Installation/Update completed."
+echo "#################################################################"
