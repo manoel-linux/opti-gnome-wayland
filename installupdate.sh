535ab91a28b99314424da1e83d59eef31d2a1d55
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-06-18 23:30:37 -0300

    Update installupdate.sh

--- installupdate.sh
+++ installupdate.sh
@@ -12,9 +12,7 @@ echo "  ██████  ██   ████  ██████  ██   
 echo "#################################################################"
 echo "opti-gnome-wayland-github: https://github.com/manoel-linux/opti-gnome-wayland"
 echo "#################################################################"
-echo "$(date)"
-echo "Linux $(uname -r)"
-echo "#################################################################"
+
 
 if [[ $EUID -ne 0 ]]; then
 echo "ERROR! sudo needed" 
